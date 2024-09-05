7a3022459421cae9204215c5fe8066e666acaaf0
MaxiiSilva <maxigabrielsilva@gmail.com>
2024-09-04 21:51:58 -0300

    primer commit

---
+++ tarea/src/App.js
@@ -0,0 +1,78 @@
+// Punto 1
+// import React from 'react';
+// import HolaMundo from './HolaMundo';
+
+// function App() {
+//     return (
+//         <div className="App">
+//             <HolaMundo />
+//         </div>
+//     );
+// }
+
+// export default App;
+
+
+// Punto 2
+// import React from 'react';
+// import TdP from './TdP';
+
+// const App = () => {
+//   return (
+//     <div>
+//       <TdP
+//         nombre="Maxi"
+//         apellido="Silva"
+//         profesion="Programador medio pelo"
+//         imagen="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYgRyXHnGOzeB3XcKVYRntw0K7Ga6u3vjJTQ&s" 
+//       />
+//     </div>
+//   );
+// };
+
+// export default App;
+
+// Punto 3
+// import React from 'react';
+// import Contador from './Contador';
+
+// const App = () => {
+//   return (
+//     <div>
+//       <Contador />
+//     </div>
+//   );
+// };
+
+// export default App;
+
+
+// PUNTO 4
+
+// import React from 'react';
+// import Listas from './Listas';
+
+// const App = () => {
+//   return (
+//     <div>
+//       <Listas />
+//     </div>
+//   );
+// };
+
+// export default App;
+
+// PUNTO 5 
+
+import React from 'react';
+import Formulario from './Componentes/Formulario';
+
+const App = () => {
+  return (
+    <div>
+      <Formulario />
+    </div>
+  );
+};
+
+export default App;
